338feb63379a34964b303193a56999ff5ba74a2b
Ariyo Ayomide <ariyoraphael1@gmail.com>
2025-04-01 07:33:41 +0200

    update composer

--- src/AppleSignInServiceProvider.php
+++ src/AppleSignInServiceProvider.php
@@ -15,7 +15,7 @@ class AppleSignInServiceProvider extends ServiceProvider
             __DIR__.'/../config/applesignin.php' => config_path('applesignin.php'),
         ], 'config');
         $this->publishes([
-            __DIR__ . '/../database/migrations/2025_04_01_000000_create_social_accounts_table.php'
+            __DIR__ . '/database/migrations/2025_04_01_000000_create_social_accounts_table.php'
             => database_path('migrations/' . date('Y_m_d_His') . '_create_social_accounts_table.php'),
         ], 'migrations');
     }
